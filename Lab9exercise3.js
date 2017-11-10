05a82c86ef1013b0429e113b045cc58f0b8aa446
SimritM <31972281+SimritM@users.noreply.github.com>
2017-11-09 22:14:31 -0500

    Add files via upload

---
+++ Lab9exercise3.js
@@ -0,0 +1,7 @@
+//This will output when the page loads
+document.write("The Rest");
+
+//This will output when a button is pressed
+function therest(){
+	document.ElementById("output3").innerHTML="Output Three Here";
+}
